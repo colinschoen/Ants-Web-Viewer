59604df02df42d0fb88ec153b9d2c826aa5efab3
colinschoen <colin@colinschoen.com>
2015-07-14 04:10:32 -0700

    more progress

--- assets/app.js
+++ assets/app.js
@@ -14,9 +14,9 @@ function drawControlPanel(food, places, ants) {
     for (var id in ants) {
         ant = ants[id];
         if (ant["cost"] > food)
-            tr.append('<td data-disabled="1" data-name="' + ant["name"] + '" id="ant_' + ant["name"]  + '" class="ant-row ant-inactive"><img class="ant-img" src="' + ant["img"] + '"> ' + ant["name"] + '<hr class="ant-row-divider" /><span class="badge ant-cost">' + ant["cost"] + '</span></td>');
+            tr.append('<td data-disabled="1" data-img="' + ant["img"] + '" data-name="' + ant["name"] + '" id="ant_' + ant["name"]  + '" class="ant-row ant-inactive"><img class="ant-img" src="' + ant["img"] + '"> ' + ant["name"] + '<hr class="ant-row-divider" /><span class="badge ant-cost">' + ant["cost"] + '</span></td>');
         else
-            tr.append('<td data-disabled="0" data-name="' + ant["name"] + '" id="ant_' + ant["name"] + '" class="ant-row"><img class="ant-img" src="' + ant["img"] + '"> ' + ant["name"] + '<hr class="ant-row-divider" /><span class="badge ant-cost">' + ant["cost"] + '</span></td>');
+            tr.append('<td data-disabled="0" data-img="' + ant["img"] + '" data-name="' + ant["name"] + '" id="ant_' + ant["name"] + '" class="ant-row"><img class="ant-img" src="' + ant["img"] + '"> ' + ant["name"] + '<hr class="ant-row-divider" /><span class="badge ant-cost">' + ant["cost"] + '</span></td>');
     }
     updateFoodCount();
     drawInitialPlaces();
@@ -36,11 +36,11 @@ function drawInitialPlaces() {
             random_ground = Math.floor(Math.random() * 3) + 1;
             if (places[i][col]["water"] == 1)
                 random_ground = "water";
-            tr.append('<td class="places-td" id="pCol' + col + '"><div class="tunnel-div"><div style="background-image: url(\'assets/tiles/sky/' + random_sky + '.png\')"class="tunnel-goc-div"></div><div style="background-image: url(\'assets/tiles/ground/' + random_ground + '.png\')" class="tunnel-goc-div"></div></div></td>');
+            tr.append('<td data-row="' + i  + '" data-col="' + col  + '" data-name="' + places[i][col]["name"]  + '" class="places-td" id="pCol' + col + '"><div class="tunnel-div"><div class="tunnel-img-container"></div><div style="background-image: url(\'assets/tiles/sky/' + random_sky + '.png\')"class="tunnel-goc-div"></div><div style="background-image: url(\'assets/tiles/ground/' + random_ground + '.png\')" class="tunnel-goc-div"></div></div></td>');
         }
         if (i == 0) {
             rowspan = rows + 1
-            tr.append('<td id="places-td" rowspan="' + rowspan + '" class="place-hive-td"></td>')
+            tr.append('<td id="hive-td" rowspan="' + rowspan + '" class="place-hive-td"></td>')
             td = tr.find('.place-hive-td');
             for (bee in places["Hive"]["insects"]) {
                 td.append('<img class="bee-img" src="assets/insects/bee.gif">');
@@ -60,6 +60,7 @@ function startGame() {
     gui = new GUI();
     gui.get_gameState();
     drawControlPanel(gui.get_food(), gui.get_places(), gui.get_antTypes());
+    gui.update();
 }
 
 
@@ -107,12 +108,20 @@ GUI.prototype.get_places = function() {
 GUI.prototype.get_food = function() {
     return this.newState["food"];
 }
-GUI.prototype.selectAnt = function(name) {
-    this.selected_ant = name;
+GUI.prototype.selectAnt = function(name, img) {
+    this.selected_ant = { name: name, img: img };
+}
+GUI.prototype.deselectAnt = function() {
+    currentSelected = this.get_selectedAnt();
+    this.selected_ant = null;
+    if (currentSelected) {
+        $('#antsTableRow').find("[data-name = '" + currentSelected["name"] + "']").removeClass("ant-selected");
+    }
 }
 GUI.prototype.get_selectedAnt = function() {
     return this.selected_ant;
 }
+
 
 
 $('#antsTableRow').on('click', ".ant-row", function() {
@@ -126,10 +135,10 @@ $('#antsTableRow').on('click', ".ant-row", function() {
     }
     currentSelected = gui.get_selectedAnt();
     if (currentSelected) {
-        $('#antsTableRow').find("[data-name = '" + currentSelected + "']").removeClass("ant-selected");
+        $('#antsTableRow').find("[data-name = '" + currentSelected["name"] + "']").removeClass("ant-selected");
     }
     $(this).addClass('ant-selected');
-    gui.selectAnt($(this).attr('data-name'));
+    gui.selectAnt($(this).attr('data-name'), $(this).attr('data-img'));
 });
 
 
@@ -154,3 +163,59 @@ $('#exitBtn').on('click', function() {
     $.post("ajax/exit");
     $.post("ajax/exit");
 });
+
+$('.places-table').on('click', '.places-td', function() {
+    //Check to see if an insect is selected
+    t = this
+    selectedAnt = gui.get_selectedAnt();
+    //Deselect ant
+    gui.deselectAnt();
+    if (!selectedAnt) {
+        swal({
+            title: "Error",
+            text: "You need to select an insect first.",
+            type: "error",
+        });
+    }
+    if (selectedAnt["food"] > gui.get_food()) {
+        swal({
+            title: "Error",
+            text: "Not enough food remains to place " + selectedAnt["name"],
+            type: "error",
+        });
+    }
+    $.ajax({
+        method: "POST",
+        url: "ajax/deploy/ant",
+        data: { pname: $(this).attr("data-name"), ant: selectedAnt["name"]},
+    })
+        .done(function(response) {
+            if (response["error"]) {
+                swal({
+                    title: "Error",
+                    text: response["error"],
+                    type: "error",
+                });
+            }        
+            else {
+                $(t).find('.tunnel-img-container').html('<img class="active-ant" src="' + selectedAnt["img"]  + '">');
+                gui.update();
+            }
+        });
+});
+
+GUI.prototype.update = function() {
+    $('.active-ant').html("");
+    places = this.get_places();
+    for (r in places) {
+        if (r == "Hive") {
+            continue;
+        }
+        for (c in places[r]) {
+            if ("type" in places[r][c]["insects"]) {
+                console.log("Adding insect: " + places[r][c]["insects"]["name"]);
+                $('.places-table').find('.places-td[data-row="' + r  + '"][data-col="' + c  + '"]').find('.tunnel-img-container').html('<img class="active-ant" src="' + places[r][c]["insects"]["img"]  + '">');
+            }
+        }
+    }
+}
--- gui.py
+++ gui.py
@@ -40,7 +40,7 @@ class GUI:
     def _init_control_panel(self, colony):
         return
 
-    def exit(self):
+    def exit(self, data=None):
         self.active = False
 
     def initialize_colony_graphics(self, colony):
@@ -68,7 +68,7 @@ class GUI:
     def get_insect_img_file(self, name):
         return INSECT_FILES[name]
 
-    def getState(self, key=None):
+    def getState(self, data=None):
         """Get our message from JSON"""
         return self.state.getState()
 
@@ -116,11 +116,11 @@ class GUI:
                 rows += 1
             if not pRow in self.places:
                 self.places[pRow] = {}
-            self.places[pRow][pCol] = { "type": "tunnel", "water": 0, "insects": {} } 
+            self.places[pRow][pCol] = { "name": name, "type": "tunnel", "water": 0, "insects": {} } 
             if "water" in name:
                 self.places[pRow][pCol]["water"] = 1
         #Add the Hive
-        self.places[colony.hive.name] = { "type": "hive", "water": 0, "insects": {} }
+        self.places[colony.hive.name] = { "name": name, "type": "hive", "water": 0, "insects": {} }
         self.places[colony.hive.name]["insects"] = []
         for bee in colony.hive.bees:
             self.insects.append(bee)
@@ -130,31 +130,77 @@ class GUI:
 
 
     def _update_control_panel(self, colony):
-        #TODO actually do something
-        return
+        """Reflect the game state in the play area."""
+
+        for name, place in colony.places.items():
+            if place.name == 'Hive':
+                continue
+
+            if place.ant is not None:
+                #Ok there is an ant that needs to be drawn here
+                pCol = self.get_place_column(name)
+                pRow = self.get_place_row(name)
+                self.places[pRow][pCol] = { "name": name, "type": "tunnel", "water": 0, "insects": {"type": place.ant.name, "img": self.get_insect_img_file(place.ant.name)} }
+            for bee in place.bees:
+                #Check to see if we are at an exit
+                for other_place in colony.places.values():
+                    if other_place.exit is place:
+                        break
+                else:
+                    other_place = colony.hive
+                    self.places[pRow][pCol][bee] = 1
+
+    def deployAnt(self, data):
+        pname, ant = data["pname"], data["ant"]
+        try:
+            print("colony.deploy_ant('{0}', '{1}')".format(pname, ant))
+            self.colony.deploy_ant(pname, ant);
+        except Exception as e:
+            print(e)
+            return { "error": str(e) }
+        self._update_control_panel(self.colony);
+        return { "success": 1 }
+
 
 
 import http.server
+import cgi
 class HttpHandler(http.server.SimpleHTTPRequestHandler):
     #Override the default do_POST method
     def log_message(self, format, *args):
         #I hate this console output so simply do nothing.
         return
+    def cgiFieldStorageToDict(self, fieldStorage):
+        """ Get a plain dictionary rather than the '.value' system used by the 
+           cgi module's native fieldStorage class. """
+        params = {}
+        for key in fieldStorage.keys():
+            params[key] = fieldStorage[key].value
+        return params
+
     def do_POST(self):
         path = self.path
         action = {
                 '/ajax/fetch/state': gui.getState,
                 '/ajax/exit': gui.exit,
+                '/ajax/deploy/ant': gui.deployAnt,
                 }.get(path) 
         if not action:
             #We could not find a valid route
             return
-        response = action()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+             'CONTENT_TYPE':self.headers['Content-Type'],
+            })
+        data = self.cgiFieldStorageToDict(form)
+        response = action(data)
         self.send_response(200)
         if response:
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            response = json.dumps(action())
+            response = json.dumps(response)
             self.wfile.write(response.encode('ascii'))
 
 import socketserver, socket
@@ -175,11 +221,11 @@ def run(*args):
     #Basic HTTP Handler
     #Handler = http.server.SimpleHTTPRequestHandler
     httpd = CustomThreadingTCPServer(("", PORT), HttpHandler)
-    print("Web Server Started on port ", PORT)
+    print("Web Server started @ localhost:" + str(PORT))
     def start_http():
         while gui.active:
             httpd.handle_request()
-        print("Web Server terminated")
+        print("Web server terminated")
     threading.Thread(target=start_http).start()
     webbrowser.open("localhost:" + str(PORT), 2)
     ants.start_with_strategy(args, gui.strategy)
